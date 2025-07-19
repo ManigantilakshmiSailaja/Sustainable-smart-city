bca4695337bc450dd9dd445762cab1c5be390902
ManigantilakshmiSailaja <manigantilakshmisailaja@gmail.com>
2025-07-19 18:13:04 +0530

    Delete routes directory

--- routes/eco_tips.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/eco", tags=["Eco Tips"])
-
-@router.get("/")
-def generate_tip(keyword: str):
-    return {
-        "tip": f"Reduce your use of {keyword} by opting for reusable alternatives whenever possible!"
-    }
